1f946a601fcae0d4ea28ce7bf003d5ab40c16bfd
Toni Lugosan <tlugosan@cequint.local>
2019-10-06 11:45:18 -0700

    lesson 07 activity admin customization

--- blogging/admin.py
+++ blogging/admin.py
@@ -1,7 +1,17 @@
 from django.contrib import admin
 from blogging.models import Post, Category
 
-# Register your models here.
-admin.site.register(Post)
-admin.site.register(Category)
+class CategoryInLine(admin.TabularInline):
+    model = Category.post.through
+    
+
+class CategoryAdmin(admin.ModelAdmin):
+    exclude = ('post',)
+
+class PostAdmin(admin.ModelAdmin):
+    inlines = [CategoryInLine,]
+
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
 
